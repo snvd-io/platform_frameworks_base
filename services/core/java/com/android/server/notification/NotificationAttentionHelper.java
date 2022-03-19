aef9d829024f1ce996d0d5e3aa62d0eaa108066b
June <june@eridan.me>
2022-03-19 17:10:28 +0200

    Support forwarding notifications from other users
    Commit history:
    
    Before Android 13:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/30ab1eae4cd03270f0dce82a67a0122df7fb7c29
    
    13: a40e627709a7
    
    Signed-off-by: June <june@eridan.me>
    Co-authored-by: inthewaves <inthewaves@pm.me>
    Co-authored-by: June <june@eridan.me>
    Co-authored-by: quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
    Co-authored-by: Pratyush <codelab@pratyush.dev>

--- services/core/java/com/android/server/notification/NotificationAttentionHelper.java
+++ services/core/java/com/android/server/notification/NotificationAttentionHelper.java
@@ -968,7 +968,7 @@ public final class NotificationAttentionHelper {
         return true;
     }
 
-    private String disableNotificationEffects(NotificationRecord record, int listenerHints) {
+    String disableNotificationEffects(NotificationRecord record, int listenerHints) {
         if (mDisableNotificationEffects) {
             return "booleanState";
         }
