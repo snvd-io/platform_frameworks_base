c025a528e2473bfdcbd883b0f2963fba38e908dc
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-08-08 19:03:37 +0300

    add option to view application error details in LogViewer app
    Adds a "Show details" item to crash and ANR (app not responding) dialogs which opens the LogViewer
    app.

--- services/core/java/com/android/server/am/AppErrors.java
+++ services/core/java/com/android/server/am/AppErrors.java
@@ -840,6 +840,7 @@ class AppErrors {
 
         ApplicationErrorReport report = new ApplicationErrorReport();
         report.packageName = r.info.packageName;
+        report.applicationInfo = r.info;
         report.installerPackageName = errState.getErrorReportReceiver().getPackageName();
         report.processName = r.processName;
         report.time = timeMillis;
