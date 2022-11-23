7204d2421ce387df05b9761826a3fab8635f453e
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-11-23 19:03:13 +0200

    allow privileged installer to search for packages across all users
    13: 358007e1b2ab

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -4174,4 +4174,13 @@ public class ApplicationPackageManager extends PackageManager {
             return null;
         }
     }
+
+    @UnsupportedAppUsage
+    public PackageInfo findPackage(String packageName, long minVersion, Bundle validSignaturesSha256) {
+        try {
+            return mPM.findPackage(packageName, minVersion, validSignaturesSha256);
+        } catch (RemoteException e) {
+            throw e.rethrowAsRuntimeException();
+        }
+    }
 }
