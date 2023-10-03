616ee1027888f1a360e4a8ddbe41e4d01286f0ac
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-10-03 11:25:07 +0300

    support showing dexopt progress during boot
    showDexoptProgressBootMessage() is called by libartservice in art repo during post-OTA dexopt.

--- services/core/java/com/android/server/pm/local/PackageManagerLocalImpl.java
+++ services/core/java/com/android/server/pm/local/PackageManagerLocalImpl.java
@@ -20,11 +20,14 @@ import android.annotation.CallSuper;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.annotation.UserIdInt;
+import android.app.ActivityManager;
 import android.content.pm.SigningDetails;
 import android.os.Binder;
 import android.os.Build;
+import android.os.RemoteException;
 import android.os.UserHandle;
 import android.util.ArrayMap;
+import android.util.Slog;
 import android.util.apk.ApkSignatureVerifier;
 
 import com.android.server.pm.Computer;
@@ -258,6 +261,21 @@ public class PackageManagerLocalImpl implements PackageManagerLocal {
             }
 
             return mFilteredPackageStates;
+        }
+    }
+
+    public void showDexoptProgressBootMessage(int percentage, int current, int total) {
+        final String TAG = "DexoptBootUI";
+
+        String msg = mService.getContext().getString(
+            com.android.internal.R.string.dexopt_progress_msg, percentage, current, total);
+
+        Slog.d(TAG, "msg: " + msg);
+
+        try {
+            ActivityManager.getService().showBootMessage(msg, true);
+        } catch (RemoteException e) {
+            Slog.e(TAG, "", e);
         }
     }
 }
