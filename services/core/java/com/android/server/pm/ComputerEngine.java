d00be4f5fae27e47c7d25e86a5817aa005899d9b
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-10-13 22:17:49 +0300

    exempt telephony service from the new package visibility restrictions
    5321abef41e5130340c9612518014dd9d7664661 restricted visibility too aggressively, which broke
    numerous telephony service APIs in secondary users when caller of those APIs is a package that's
    not present in the primary user.

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -2519,6 +2519,11 @@ public class ComputerEngine implements Computer {
                 return false;
             }
         }
+
+        if (UserHandle.getAppId(callingUid) == Process.PHONE_UID) {
+            return false;
+        }
+
         // if we're in an isolated process, get the real calling UID
         if (Process.isIsolated(callingUid)) {
             callingUid = getIsolatedOwner(callingUid);
@@ -2665,6 +2670,9 @@ public class ComputerEngine implements Computer {
             return true;
         }
         if (isSystemOrRootOrShell(callingUid)) {
+            return false;
+        }
+        if (UserHandle.getAppId(callingUid) == Process.PHONE_UID) {
             return false;
         }
         final ArraySet<PackageStateInternal> packageStates =
