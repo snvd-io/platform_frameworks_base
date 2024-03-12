61fad66fe3777a7ae489b95aa15350494dc4601f
Matías Hernández <matiashe@google.com>
2024-03-12 16:51:58 +0100

    Fix allowlist token issues
    1) Don't accept enqueued notifications with an unexpected token.
    2) Ensure allowlist token matches for all parceled and unparceled notifications (by only using the "root notification" one).
    3) Simplify cookie usage in allowlist token serialization.
    4) Ensure group summary (and any notifications added directly by NMS) have the correct token.
    
    Bug: 328254922
    Bug: 305695605
    Test: atest NotificationManagerServiceTest ParcelTest CloseSystemDialogsTest + manually
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:390b7840ee8738377758f9750fd6d462ea55b8ee)
    Merged-In: I232e9b74eece745560ed2e762071b48984b3f176
    Change-Id: I232e9b74eece745560ed2e762071b48984b3f176

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -2642,8 +2642,11 @@ public class Notification implements Parcelable
         if (mAllowlistToken == null) {
             mAllowlistToken = processAllowlistToken;
         }
-        // Propagate this token to all pending intents that are unmarshalled from the parcel.
-        parcel.setClassCookie(PendingIntent.class, mAllowlistToken);
+        // Propagate this token to all pending intents that are unmarshalled from the parcel,
+        // or keep the one we're already propagating, if that's the case.
+        if (!parcel.hasClassCookie(PendingIntent.class)) {
+            parcel.setClassCookie(PendingIntent.class, mAllowlistToken);
+        }
 
         when = parcel.readLong();
         creationTime = parcel.readLong();
@@ -3177,9 +3180,24 @@ public class Notification implements Parcelable
             PendingIntent.addOnMarshaledListener(addedListener);
         }
         try {
-            // IMPORTANT: Add marshaling code in writeToParcelImpl as we
-            // want to intercept all pending events written to the parcel.
-            writeToParcelImpl(parcel, flags);
+            boolean mustClearCookie = false;
+            if (!parcel.hasClassCookie(Notification.class)) {
+                // This is the "root" notification, and not an "inner" notification (including
+                // publicVersion or anything else that might be embedded in extras). So we want
+                // to use its token for every inner notification (might be null).
+                parcel.setClassCookie(Notification.class, mAllowlistToken);
+                mustClearCookie = true;
+            }
+            try {
+                // IMPORTANT: Add marshaling code in writeToParcelImpl as we
+                // want to intercept all pending events written to the parcel.
+                writeToParcelImpl(parcel, flags);
+            } finally {
+                if (mustClearCookie) {
+                    parcel.removeClassCookie(Notification.class, mAllowlistToken);
+                }
+            }
+
             synchronized (this) {
                 // Must be written last!
                 parcel.writeArraySet(allPendingIntents);
@@ -3194,7 +3212,10 @@ public class Notification implements Parcelable
     private void writeToParcelImpl(Parcel parcel, int flags) {
         parcel.writeInt(1);
 
-        parcel.writeStrongBinder(mAllowlistToken);
+        // Always use the same token as the root notification (might be null).
+        IBinder rootNotificationToken = (IBinder) parcel.getClassCookie(Notification.class);
+        parcel.writeStrongBinder(rootNotificationToken);
+
         parcel.writeLong(when);
         parcel.writeLong(creationTime);
         if (mSmallIcon == null && icon != 0) {
@@ -3594,6 +3615,11 @@ public class Notification implements Parcelable
         if (publicVersion != null) {
             publicVersion.overrideAllowlistToken(token);
         }
+    }
+
+    /** @hide */
+    public IBinder getAllowlistToken() {
+        return mAllowlistToken;
     }
 
     /**
