e97565f1da1e769ada90e804c37ef7e8c335ee92
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-04-30 19:14:11 +0300

    rename AppsFilterImpl.grantImplicitAccess method
    This is done to have the build break in case grantImplicitAccess() starts to get used in more
    places, which might weaken AppsFilter-based app isolation via PackageHooks.

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -7541,7 +7541,7 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
             accessGranted = mInstantAppRegistry.grantInstantAccess(userId, intent,
                     recipientAppId, UserHandle.getAppId(visibleUid) /*instantAppId*/);
         } else {
-            accessGranted = mAppsFilter.grantImplicitAccess(recipientUid, visibleUid,
+            accessGranted = mAppsFilter.grantImplicitAccess2(recipientUid, visibleUid,
                     retainOnUpdate);
         }
 
