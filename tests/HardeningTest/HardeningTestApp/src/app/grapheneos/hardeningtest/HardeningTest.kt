GrapheneOS-Archive/platform_frameworks_base-old

add a test for the new per-app hardening features
Currently covers restriction of dynamic code execution from memory/storage and blocking of ptrace
access.

Testing is performed on three kinds of HardeningTestApp: targetSdk 27, current targetSdk and
preinstalled current targetSdk app (the latter is included on debuggable builds).

Preinstalled app is tested separately to make sure that restrictions can't be weakened for it,
regardless of GosPackageState values.

To run the test, execute `atest HardeningTest`.

tests/HardeningTest/HardeningTestApp/src/app/grapheneos/hardeningtest/HardeningTest.kt
@@ -1,0 +1,101 @@
+package app.grapheneos.hardeningtest
+
+import android.app.Application
+import android.content.Intent
+import android.os.Environment
+import android.os.ParcelFileDescriptor
+import android.os.Process
+import android.util.Log
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.rule.ServiceTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import kotlin.reflect.KClass
+
+private val TAG = HardeningTest::class.java.simpleName
+
+@RunWith(AndroidJUnit4::class)
+class HardeningTest {
+    val ctx = ApplicationProvider.getApplicationContext<Application>()
+    val mainProcessPid = Process.myPid()
+
+    @Rule @JvmField
+    val serviceRule = ServiceTestRule()
+    @Rule @JvmField
+    val isolatedServiceRule = ServiceTestRule()
+
+    lateinit var service: ITestService
+    lateinit var isolatedService: ITestService
+
+    @Before
+    fun bindServices() {
+        isolatedService = bindService(isolatedServiceRule, IsolatedTestService::class)
+        service = bindService(serviceRule, TestService::class)
+    }
+
+    private fun testDynamicCodeExecution(svc: ITestService, isAllowed: Boolean, type: MultiTests.Type) {
+        Assert.assertEquals("Environment.isExecmemBlocked()",
+            !isAllowed, Environment.isExecmemBlocked())
+
+        svc.testDynamicCodeExecution(
+            type.name,
+            isAllowed,
+            ParcelFileDescriptor.adoptFd(Utils.getFdForExecAppDataFileTest(ctx)),
+            ParcelFileDescriptor.adoptFd(Utils.getFdForExecmodTest(ctx)),
+        )?.let {
+            Assert.fail(it)
+        }
+    }
+
+    @Test
+    fun testMemoryDceAllowed() = testDynamicCodeExecution(service, true, MultiTests.Type.MemoryDce)
+
+    @Test
+    fun testMemoryDceAllowedIsolated() = testDynamicCodeExecution(isolatedService, true, MultiTests.Type.MemoryDce)
+
+    @Test
+    fun testMemoryDceRestricted() = testDynamicCodeExecution(service, false, MultiTests.Type.MemoryDce)
+
+    @Test
+    fun testMemoryDceRestrictedIsolated() = testDynamicCodeExecution(isolatedService, false, MultiTests.Type.MemoryDce)
+
+    @Test
+    fun testStorageDceAllowed() = testDynamicCodeExecution(service, true, MultiTests.Type.StorageDce)
+
+    @Test
+    fun testStorageDceAllowedIsolated() = testDynamicCodeExecution(isolatedService, true, MultiTests.Type.StorageDce)
+
+    @Test
+    fun testStorageDceRestricted() = testDynamicCodeExecution(service, false, MultiTests.Type.StorageDce)
+
+    @Test
+    fun testStorageDceRestrictedIsolated() = testDynamicCodeExecution(isolatedService, false, MultiTests.Type.StorageDce)
+
+    private fun testPtrace(svc: ITestService, isAllowed: Boolean) {
+        svc.testPtrace(isAllowed, mainProcessPid)?.let {
+            Assert.fail(it)
+        }
+    }
+
+    @Test
+    fun testPtraceAllowed() = testPtrace(service, true)
+
+    @Test
+    fun testPtraceAllowedIsolated() = testPtrace(isolatedService, true)
+
+    @Test
+    fun testPtraceDenied() = testPtrace(service, false)
+
+    @Test
+    fun testPtraceDeniedIsolated() = testPtrace(isolatedService, false)
+
+    private fun bindService(rule: ServiceTestRule, cls: KClass<*>): ITestService {
+        val binder = rule.bindService(Intent(ctx, cls.java))
+        val svc = ITestService.Stub.asInterface(binder)
+        return svc
+    }
+}

tests/HardeningTest/HardeningTestApp/src/app/grapheneos/hardeningtest/ITestService.aidl
@@ -1,0 +1,8 @@
+package app.grapheneos.hardeningtest;
+
+import android.os.ParcelFileDescriptor;
+
+interface ITestService {
+    @nullable String testDynamicCodeExecution(String type, boolean isAllowed, in ParcelFileDescriptor appDataFileFd, in ParcelFileDescriptor execmodFd);
+    @nullable String testPtrace(boolean isAllowed, int mainProcessPid);
+}

tests/HardeningTest/HardeningTestApp/src/app/grapheneos/hardeningtest/MultiTests.java
@@ -1,0 +1,70 @@
+package app.grapheneos.hardeningtest;
+
+import android.app.ActivityThread;
+import android.system.ErrnoException;
+import android.system.Os;
+import android.system.OsConstants;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+
+import static app.grapheneos.hardeningtest.MultiTests.Type.MemoryDce;
+import static app.grapheneos.hardeningtest.MultiTests.Type.StorageDce;
+
+public class MultiTests {
+    static {
+        System.loadLibrary("hardening_test");
+    }
+
+    enum Type {
+        MemoryDce,
+        StorageDce,
+    }
+
+    @MultiTest(type = MemoryDce)
+    public static native int execmem();
+
+    @MultiTest(type = MemoryDce, alwaysDeniedMinSdk = 28, alwaysDeniedMinSdkIsolated = 1, skipAllowedTest = true)
+    public static native int execmod(int fd);
+
+    // no @MultiTest, special-cased in test runner
+    public static native int ptrace(int pid);
+
+    @MultiTest(type = MemoryDce)
+    public static native int exec_appdomain_tmpfs();
+
+    @MultiTest(type = MemoryDce, alwaysDeniedMinSdk = 28, alwaysDeniedMinSdkIsolated = 1)
+    public static native int execute_ashmem();
+
+    @MultiTest(type = MemoryDce)
+    public static native int execute_ashmem_libcutils();
+
+    // successful allowedTest never returns, skip it
+    @MultiTest(type = StorageDce, skipAllowedTest = true)
+    public static native int exec_app_data_file(int fd);
+
+    @MultiTest(type = StorageDce, skipAllowedTest = true)
+    public static int exec_app_data_file_path() throws IOException {
+        var ctx = ActivityThread.currentApplication();
+        File file = new File(ctx.getFilesDir(), "self_exe_" + System.nanoTime());
+
+        try {
+            Files.copy(Paths.get("/proc/self/exe"), file.toPath());
+
+            FileDescriptor fd = Os.open(file.getPath(), OsConstants.O_RDONLY, 0);
+            Os.execv("/proc/self/fd/" + fd.getInt$(), new String[0]);
+            return 0;
+        } catch (NoSuchFileException | AccessDeniedException e) {
+            return OsConstants.EACCES;
+        } catch (ErrnoException e) {
+            return e.errno;
+        } finally {
+            file.delete();
+        }
+    }
+}

tests/HardeningTest/HardeningTestApp/src/app/grapheneos/hardeningtest/TestService.java
@@ -1,0 +1,135 @@
+package app.grapheneos.hardeningtest;
+
+import android.annotation.Nullable;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.ParcelFileDescriptor;
+import android.os.Process;
+import android.os.SELinux;
+import android.system.OsConstants;
+import android.util.Log;
+
+import java.lang.reflect.Method;
+
+import static app.grapheneos.hardeningtest.Utils.getTargetSdk;
+import static junit.framework.Assert.assertEquals;
+
+public class TestService extends Service {
+
+    public void onCreate() {
+        super.onCreate();
+        Utils.checkSELinuxContextAndFlags(getExpectedSELinuxContextPrefix(), getTargetSdk(this));
+    }
+
+    protected String getExpectedSELinuxContextPrefix() {
+        int targetSdk = getTargetSdk(this);
+        if (targetSdk != Build.VERSION.SDK_INT) {
+            assertEquals("targetSdk", 27, targetSdk);
+            return "u:r:untrusted_app_27:s0:c";
+        }
+        return "u:r:untrusted_app:s0:c";
+    }
+
+    class BinderImpl extends ITestService.Stub {
+        @Override
+        @Nullable
+        public String testDynamicCodeExecution(String typeStr, boolean isAllowed, ParcelFileDescriptor appDataFileFd, ParcelFileDescriptor execmodFd) {
+            boolean isIsolated = Process.isIsolated();
+            MultiTests.Type type = MultiTests.Type.valueOf(typeStr);
+
+            var failures = new StringBuilder();
+
+            for (Method m : MultiTests.class.getDeclaredMethods()) {
+                var ann = m.getAnnotation(MultiTest.class);
+                if (ann == null) {
+                    continue;
+                }
+
+                if (ann.type() != type) {
+                    continue;
+                }
+
+                int targetSdk = getApplicationInfo().targetSdkVersion;
+
+                boolean blockedByBasePolicy = targetSdk >=
+                    (isIsolated ? ann.alwaysDeniedMinSdkIsolated() : ann.alwaysDeniedMinSdk());
+
+                if (!blockedByBasePolicy && isAllowed && ann.skipAllowedTest()) {
+                    continue;
+                }
+
+                Object[] args;
+                switch (m.getName()) {
+                    case "execmod":
+                        args = new Object[] { execmodFd.detachFd() };
+                        break;
+                    case "exec_app_data_file":
+                        args = new Object[] { appDataFileFd.detachFd() };
+                        break;
+                    default:
+                        args = new Object[0];
+                }
+                int ret;
+                try {
+                    ret = (int) m.invoke(null, args);
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                    throw new IllegalStateException(e);
+                }
+
+                Log.d("testDce" + (isAllowed? "_allowed" : "_restricted"), "isolated " + isIsolated
+                    +", targetSdk " + targetSdk + " " + m.getName() + ": " + Errno.name(ret));
+
+                int expectedRet = isAllowed?
+                    isIsolated ?
+                        (blockedByBasePolicy? ann.blockedReturnCodeIsolated() : ann.allowedReturnCodeIsolated()) :
+                        (blockedByBasePolicy? ann.blockedReturnCode() : ann.allowedReturnCode()) :
+                    isIsolated ?
+                        ann.blockedReturnCodeIsolated() : ann.blockedReturnCode();
+
+                if (ret != expectedRet) {
+                    failures.append('\n');
+                    failures.append(m.getName());
+                    failures.append(": expected ");
+                    failures.append(Errno.name(expectedRet));
+                    failures.append(", got ");
+                    failures.append(Errno.name(ret));
+                    failures.append(", ");
+                    failures.append(getProcInfo());
+                }
+            }
+
+            if (failures.length() != 0) {
+                return failures.toString();
+            }
+
+            return null;
+        }
+
+        private String getProcInfo() {
+            return "SELinux context: " + SELinux.getContext() + ", pkg: " + getPackageName();
+        }
+
+        @Override
+        @Nullable
+        public String testPtrace(boolean isAllowed, int mainProcessPid) {
+            int ret = MultiTests.ptrace(mainProcessPid);
+            int expectedRet = isAllowed ? 0 : OsConstants.EPERM;
+
+            if (ret != expectedRet) {
+                return "expected " + Errno.name(expectedRet) + ", got " + Errno.name(ret) + ", " + getProcInfo();
+            }
+
+            return null;
+        }
+    }
+
+    private final IBinder binder = new BinderImpl();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+}

snvd-io/platform_frameworks_base

tests/HardeningTest/HardeningTestApp/src/app/grapheneos/hardeningtest/HardeningTest.kt
@@ -1,0 +1,100 @@
+package app.grapheneos.hardeningtest
+
+import android.app.Application
+import android.content.Intent
+import android.os.Environment
+import android.os.ParcelFileDescriptor
+import android.os.Process
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.rule.ServiceTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import kotlin.reflect.KClass
+
+private val TAG = HardeningTest::class.java.simpleName
+
+@RunWith(AndroidJUnit4::class)
+class HardeningTest {
+    val ctx = ApplicationProvider.getApplicationContext<Application>()
+    val mainProcessPid = Process.myPid()
+
+    @Rule @JvmField
+    val serviceRule = ServiceTestRule()
+    @Rule @JvmField
+    val isolatedServiceRule = ServiceTestRule()
+
+    lateinit var service: ITestService
+    lateinit var isolatedService: ITestService
+
+    @Before
+    fun bindServices() {
+        isolatedService = bindService(isolatedServiceRule, IsolatedTestService::class)
+        service = bindService(serviceRule, TestService::class)
+    }
+
+    private fun testDynamicCodeLoading(svc: ITestService, isAllowed: Boolean, type: MultiTests.Type) {
+        Assert.assertEquals("Environment.isExecmemBlocked()",
+            !isAllowed, Environment.isExecmemBlocked())
+
+        svc.testDynamicCodeLoading(
+            type.name,
+            isAllowed,
+            ParcelFileDescriptor.adoptFd(Utils.getFdForExecAppDataFileTest(ctx)),
+            ParcelFileDescriptor.adoptFd(Utils.getFdForExecmodTest(ctx)),
+        )?.let {
+            Assert.fail(it)
+        }
+    }
+
+    @Test
+    fun testMemoryDclAllowed() = testDynamicCodeLoading(service, true, MultiTests.Type.MemoryDcl)
+
+    @Test
+    fun testMemoryDclAllowedIsolated() = testDynamicCodeLoading(isolatedService, true, MultiTests.Type.MemoryDcl)
+
+    @Test
+    fun testMemoryDclRestricted() = testDynamicCodeLoading(service, false, MultiTests.Type.MemoryDcl)
+
+    @Test
+    fun testMemoryDclRestrictedIsolated() = testDynamicCodeLoading(isolatedService, false, MultiTests.Type.MemoryDcl)
+
+    @Test
+    fun testStorageDclAllowed() = testDynamicCodeLoading(service, true, MultiTests.Type.StorageDcl)
+
+    @Test
+    fun testStorageDclAllowedIsolated() = testDynamicCodeLoading(isolatedService, true, MultiTests.Type.StorageDcl)
+
+    @Test
+    fun testStorageDclRestricted() = testDynamicCodeLoading(service, false, MultiTests.Type.StorageDcl)
+
+    @Test
+    fun testStorageDclRestrictedIsolated() = testDynamicCodeLoading(isolatedService, false, MultiTests.Type.StorageDcl)
+
+    private fun testPtrace(svc: ITestService, isAllowed: Boolean) {
+        svc.testPtrace(isAllowed, mainProcessPid)?.let {
+            Assert.fail(it)
+        }
+    }
+
+    @Test
+    fun testPtraceAllowed() = testPtrace(service, true)
+
+    @Test
+    fun testPtraceAllowedIsolated() = testPtrace(isolatedService, true)
+
+    @Test
+    fun testPtraceDenied() = testPtrace(service, false)
+
+    @Test
+    fun testPtraceDeniedIsolated() = testPtrace(isolatedService, false)
+
+    private fun bindService(rule: ServiceTestRule, cls: KClass<*>): ITestService {
+        val binder = rule.bindService(Intent(ctx, cls.java))
+        val svc = ITestService.Stub.asInterface(binder)
+        return svc
+    }
+}

tests/HardeningTest/HardeningTestApp/src/app/grapheneos/hardeningtest/ITestService.aidl
@@ -1,0 +1,8 @@
+package app.grapheneos.hardeningtest;
+
+import android.os.ParcelFileDescriptor;
+
+interface ITestService {
+    @nullable String testDynamicCodeLoading(String type, boolean isAllowed, in ParcelFileDescriptor appDataFileFd, in ParcelFileDescriptor execmodFd);
+    @nullable String testPtrace(boolean isAllowed, int mainProcessPid);
+}

tests/HardeningTest/HardeningTestApp/src/app/grapheneos/hardeningtest/MultiTests.java
@@ -1,0 +1,70 @@
+package app.grapheneos.hardeningtest;
+
+import android.app.ActivityThread;
+import android.system.ErrnoException;
+import android.system.Os;
+import android.system.OsConstants;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+
+import static app.grapheneos.hardeningtest.MultiTests.Type.MemoryDcl;
+import static app.grapheneos.hardeningtest.MultiTests.Type.StorageDcl;
+
+public class MultiTests {
+    static {
+        System.loadLibrary("hardening_test");
+    }
+
+    enum Type {
+        MemoryDcl,
+        StorageDcl,
+    }
+
+    @MultiTest(type = MemoryDcl)
+    public static native int execmem();
+
+    @MultiTest(type = MemoryDcl, alwaysDeniedMinSdk = 28, alwaysDeniedMinSdkIsolated = 1, skipAllowedTest = true)
+    public static native int execmod(int fd);
+
+    // no @MultiTest, special-cased in test runner
+    public static native int ptrace(int pid);
+
+    @MultiTest(type = MemoryDcl)
+    public static native int exec_appdomain_tmpfs();
+
+    @MultiTest(type = MemoryDcl, alwaysDeniedMinSdk = 28, alwaysDeniedMinSdkIsolated = 1)
+    public static native int execute_ashmem();
+
+    @MultiTest(type = MemoryDcl)
+    public static native int execute_ashmem_libcutils();
+
+    // successful allowedTest never returns, skip it
+    @MultiTest(type = StorageDcl, skipAllowedTest = true)
+    public static native int exec_app_data_file(int fd);
+
+    @MultiTest(type = StorageDcl, skipAllowedTest = true)
+    public static int exec_app_data_file_path() throws IOException {
+        var ctx = ActivityThread.currentApplication();
+        File file = new File(ctx.getFilesDir(), "self_exe_" + System.nanoTime());
+
+        try {
+            Files.copy(Paths.get("/proc/self/exe"), file.toPath());
+
+            FileDescriptor fd = Os.open(file.getPath(), OsConstants.O_RDONLY, 0);
+            Os.execv("/proc/self/fd/" + fd.getInt$(), new String[0]);
+            return 0;
+        } catch (NoSuchFileException | AccessDeniedException e) {
+            return OsConstants.EACCES;
+        } catch (ErrnoException e) {
+            return e.errno;
+        } finally {
+            file.delete();
+        }
+    }
+}

tests/HardeningTest/HardeningTestApp/src/app/grapheneos/hardeningtest/TestService.java
@@ -1,0 +1,134 @@
+package app.grapheneos.hardeningtest;
+
+import android.annotation.Nullable;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.ParcelFileDescriptor;
+import android.os.Process;
+import android.os.SELinux;
+import android.system.OsConstants;
+import android.util.Log;
+
+import java.lang.reflect.Method;
+
+import static app.grapheneos.hardeningtest.Utils.getTargetSdk;
+import static junit.framework.Assert.assertEquals;
+
+public class TestService extends Service {
+
+    public void onCreate() {
+        super.onCreate();
+        Utils.checkSELinuxContextAndFlags(getExpectedSELinuxContextPrefix(), getTargetSdk(this));
+    }
+
+    protected String getExpectedSELinuxContextPrefix() {
+        int targetSdk = getTargetSdk(this);
+        if (targetSdk != Build.VERSION.SDK_INT) {
+            assertEquals("targetSdk", 27, targetSdk);
+            return "u:r:untrusted_app_27:s0:c";
+        }
+        return "u:r:untrusted_app:s0:c";
+    }
+
+    class BinderImpl extends ITestService.Stub {
+        @Nullable
+        public String testDynamicCodeLoading(String typeStr, boolean isAllowed, ParcelFileDescriptor appDataFileFd, ParcelFileDescriptor execmodFd) {
+            boolean isIsolated = Process.isIsolated();
+            MultiTests.Type type = MultiTests.Type.valueOf(typeStr);
+
+            var failures = new StringBuilder();
+
+            for (Method m : MultiTests.class.getDeclaredMethods()) {
+                var ann = m.getAnnotation(MultiTest.class);
+                if (ann == null) {
+                    continue;
+                }
+
+                if (ann.type() != type) {
+                    continue;
+                }
+
+                int targetSdk = getApplicationInfo().targetSdkVersion;
+
+                boolean blockedByBasePolicy = targetSdk >=
+                    (isIsolated ? ann.alwaysDeniedMinSdkIsolated() : ann.alwaysDeniedMinSdk());
+
+                if (!blockedByBasePolicy && isAllowed && ann.skipAllowedTest()) {
+                    continue;
+                }
+
+                Object[] args;
+                switch (m.getName()) {
+                    case "execmod":
+                        args = new Object[] { execmodFd.detachFd() };
+                        break;
+                    case "exec_app_data_file":
+                        args = new Object[] { appDataFileFd.detachFd() };
+                        break;
+                    default:
+                        args = new Object[0];
+                }
+                int ret;
+                try {
+                    ret = (int) m.invoke(null, args);
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                    throw new IllegalStateException(e);
+                }
+
+                Log.d("testDcl" + (isAllowed? "_allowed" : "_restricted"), "isolated " + isIsolated
+                    +", targetSdk " + targetSdk + " " + m.getName() + ": " + Errno.name(ret));
+
+                int expectedRet = isAllowed?
+                    isIsolated ?
+                        (blockedByBasePolicy? ann.blockedReturnCodeIsolated() : ann.allowedReturnCodeIsolated()) :
+                        (blockedByBasePolicy? ann.blockedReturnCode() : ann.allowedReturnCode()) :
+                    isIsolated ?
+                        ann.blockedReturnCodeIsolated() : ann.blockedReturnCode();
+
+                if (ret != expectedRet) {
+                    failures.append('\n');
+                    failures.append(m.getName());
+                    failures.append(": expected ");
+                    failures.append(Errno.name(expectedRet));
+                    failures.append(", got ");
+                    failures.append(Errno.name(ret));
+                    failures.append(", ");
+                    failures.append(getProcInfo());
+                }
+            }
+
+            if (failures.length() != 0) {
+                return failures.toString();
+            }
+
+            return null;
+        }
+
+        private String getProcInfo() {
+            return "SELinux context: " + SELinux.getContext() + ", pkg: " + getPackageName();
+        }
+
+        @Override
+        @Nullable
+        public String testPtrace(boolean isAllowed, int mainProcessPid) {
+            int ret = MultiTests.ptrace(mainProcessPid);
+            int expectedRet = isAllowed ? 0 : OsConstants.EPERM;
+
+            if (ret != expectedRet) {
+                return "expected " + Errno.name(expectedRet) + ", got " + Errno.name(ret) + ", " + getProcInfo();
+            }
+
+            return null;
+        }
+    }
+
+    private final IBinder binder = new BinderImpl();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+}